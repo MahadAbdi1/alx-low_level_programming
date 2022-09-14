bf8ceee372dc3ddca2faf28ca61a570a48366f28
MahadAbdi1 <219122822@mycput.ac.za>
2022-09-14 08:26:19 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,16 +1,12 @@
-#include “main.h”
-/**
-* print_alphabet – function to print abc
-*
-* Return: 0
+/*** print_alphabet – function to print abc** Return: 0
 */
+
 Void print_alphabet(void)
 {
 Char c;
 
 For (c = ‘a’; c <= ‘z’; c++)
 {
-_putchar(c);
+_putchar c;
 }
 _putchar(‘\n’);
-}
