3db2ca3aaeba0083f7f763ea549d6ff1e247a034
MahadAbdi1 <219122822@mycput.ac.za>
2022-09-19 12:15:40 -0700

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,20 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/*
+ * main: Header file containing prototypes for all functions
+ */
+void reset_to_98(int *n);
+void swap_int(int *a, int *b);
+void print_rev(char *s);
+void rev_string(char *s);
+void _puts(char *str);
+void puts_half(char *str);
+void puts2(char *str);
+void print_array(int *a, int n);
+int _strlen(char *s);
+int _putchar(char);
+int _atoi(char *s);
+char *_strcpy(char *dest, char *src);
+
+#endif
